407f5bad4aac22635472399f38d13b68c712fa87
yunmingchun <yunmingchun@gmail.com>
2024-06-11 10:47:19 +0800

    fix: .env.*

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
     // dev 独有配置
     return {
         root, //项目根目录（index.html 文件所在的位置） 默认： process.cwd()
-        base: '/', //  开发或生产环境服务的公共基础路径：默认'/'
+        base: './', //  开发或生产环境服务的公共基础路径：默认'/'
         publicDir: resolve(__dirname, './public'), //默认'public'  作为静态资源服务的文件夹  (打包public文件夹会没有，里面得东西会直接编译在dist文件下)
         assetsInclude: resolve(__dirname, './src/assets'), // 静态资源处理
         // ******插件配置******
